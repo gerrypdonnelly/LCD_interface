356e511873c500150f6c0b3cc8ef01830eb26f97
Ger <gerrypdonnelly@gmail.com>
2024-12-18 22:21:11 +0000

    Updated ports in main.c

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 /**
  ******************************************************************************
-Using push button to turn off and on led with BSRR
-Key PA0
-LED PB2 APB2
+Connecting to LCD and sending data from stm32f103xb
+Connections
+DB0	PA0
+DB1	PA1
+DB2	PA2
+DB3 PA3
+DB4 PA4
+DB5 PA5
+DB6 PA6
+DB7 PA7
+
+PC13 RS
+PC14 R/W
+PC15 E 
  ******************************************************************************
  */
 
@@ -12,35 +23,17 @@ LED PB2 APB2
 
 int main(void)
 {
-RCC->APB2ENR |=(1U<<3);//Enable clock to PB
-//Set Mode of LED pin to output max speed 50Mhz
-GPIOB->CRL |=(1U<<8);
-GPIOB->CRL |=(1U<<9);
-//Set CNF of LED pin to Output push pull
-GPIOB->CRL &=~(1U<<10);
-GPIOB->CRL &=~(1U<<11);
-
-RCC->APB2ENR |= (1U<<2);//Enable clock to PA
-//Set Mode of button to input
-GPIOA->CRL |=(1U<<3);
-GPIOA->CRL &=~(1U<<2);
-//Set CNF of BUTTON pin to input
-GPIOA->CRL &=~(1U<<1);
-GPIOA->CRL &=~(1U<<0);
-//SET PUll down resistor of BUTTON pin by setting ODR to 1
-GPIOA->ODR &=~(1U<<0);
+RCC->APB2ENR |=(1U<<);//Enable clock to PA
+//Set Mode of LCD pins to output max speed 50Mhz
+GPIOB->CRL |=(1U<<);
+GPIOB->CRL |=(1U<<);
+//Set CNF of LCD pins to Output push pull
+GPIOB->CRL &=~(1U<<);
+GPIOB->CRL &=~(1U<<);
 
 while(1)
 {
-	//Wait for input to be high on PA0
-	if(GPIOA->IDR & (1U<<0))
-	{
-		GPIOB->BSRR |=(1U<<2);
-	}
-	else
-	{
-		GPIOB->BSRR |=(1U<<18);
-	}
+
 }
 }
 
